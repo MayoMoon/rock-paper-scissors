9fd999954f1221bb8a74df4ad0dde79231886f56
MayoMoon <112760278+MayoMoon@users.noreply.github.com>
2023-08-09 06:31:20 +0100

    creates getComputerChoice function

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Random number generator
+  function getComputerChoice() {
+  const x = Math.floor(Math.random()*3 )+1;
+  return x;
+  console.log(x);
+  }
+
+  console.log(getComputerChoice());
